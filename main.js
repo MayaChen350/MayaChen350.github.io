78326e02c8e8e528c0d8d92611e61a148ac5d7c3
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-01-17 01:24:10 -0500

    added some javascript
    thanks javascript for not letting me do what I wanted to do 👏

---
+++ main.js
@@ -0,0 +1,28 @@
+const socialMedias = ["GITHUB: MayaChen350", "DISCORD: giratina_shiny"];
+const socialMediaPfps = ["images/PFP/github.jfif", "images/PFP/discord.webp"];
+
+const mediaUsername = document.querySelector("#social-media-username");
+const mediaPfp = document.querySelector("#pfp");
+let indexMedia = 0;
+
+const socialMediaChangeTimer = setInterval(() => {
+  indexMedia = indexMedia !== socialMedias.length - 1 ? indexMedia + 1 : 0;
+  mediaPfp.src = socialMediaPfps[indexMedia];
+  mediaUsername.innerText = socialMedias[indexMedia];
+
+  //   mediaPfp.style.opacity = 1;
+  //   let gone = false;
+  //   let timer = setInterval(() => {
+  //     if (mediaPfp.style.opacity < 0.1 && !gone) {
+  //       indexMedia = indexMedia !== socialMedias.length - 1 ? indexMedia + 1 : 0;
+  //       mediaPfp.src = socialMediaPfps[indexMedia];
+  //       gone = true;
+  //     } else if (!gone) {
+  //       mediaPfp.style.opacity -= 0.1;
+  //     } else if (mediaPfp.style.opacity === 1) {
+  //       mediaUsername.innerText = socialMedias[indexMedia];
+  //       clearInterval(timer, socialMediaChangeTimer);
+  //       gone = false;
+  //     } else mediaPfp.style.opacity += 0.1;
+  //   }, 100);
+}, 10000);
