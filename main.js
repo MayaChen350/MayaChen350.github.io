3395f8e4a8fb9a839e23eac042221e54256db9ec
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-02-16 16:24:03 -0500

    Made a "now playing" thingie using last.fm API

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const socialMedias = ["GITHUB: MayaChen350", "DISCORD: giratina_shiny","LAST.FM: GChen3843", "BLUESKY: giratina_shiny"];
-const socialMediaPfps = ["images/PFP/github.jfif", "images/PFP/discord.webp", "images/PFP/lastfm.png", "images/PFP/bluesky.png"];
+const socialMedias = [
+  "GITHUB: MayaChen350",
+  "DISCORD: giratina_shiny",
+  "LAST.FM: GChen3843",
+  "BLUESKY: giratina_shiny",
+];
+const socialMediaPfps = [
+  "images/PFP/github.jfif",
+  "images/PFP/discord.webp",
+  "images/PFP/lastfm.png",
+  "images/PFP/bluesky.png",
+];
 
 const mediaUsername = document.querySelector("#social-media-username");
 const mediaPfp = document.querySelector("#pfp");
@@ -9,28 +19,38 @@ const socialMediaChangeTimer = setInterval(() => {
   indexMedia = indexMedia !== socialMedias.length - 1 ? indexMedia + 1 : 0;
   mediaPfp.src = socialMediaPfps[indexMedia];
   mediaUsername.innerText = socialMedias[indexMedia];
-
-  //   mediaPfp.style.opacity = 1;
-  //   let gone = false;
-  //   let timer = setInterval(() => {
-  //     if (mediaPfp.style.opacity < 0.1 && !gone) {
-  //       indexMedia = indexMedia !== socialMedias.length - 1 ? indexMedia + 1 : 0;
-  //       mediaPfp.src = socialMediaPfps[indexMedia];
-  //       gone = true;
-  //     } else if (!gone) {
-  //       mediaPfp.style.opacity -= 0.1;
-  //     } else if (mediaPfp.style.opacity === 1) {
-  //       mediaUsername.innerText = socialMedias[indexMedia];
-  //       clearInterval(timer, socialMediaChangeTimer);
-  //       gone = false;
-  //     } else mediaPfp.style.opacity += 0.1;
-  //   }, 100);
 }, 10000);
 
-// Persona 3 reload
-// babybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybabybaby
-const currentDate = new Date();
-const p3rDate = new Date("2024/02/02 00:00:00");
-const countdown = document.querySelector("#countdown");
+// Last song listened to, powered by Last.fm (My beloved)
+const albumImage = document.querySelector("#album-image");
 
-countdown.innerText = Math.ceil((p3rDate - currentDate) / 60000);
+const timeListened = document.querySelector("#time-of-listen");
+const recentTrackName = document.querySelector("#recent-song-track");
+const recentTrackArtist = document.querySelector("#recent-song-artist");
+const trackUrl = document.querySelector("#track-url");
+
+const url =
+  "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=GChen3843&api_key=d7b26ab026668c5044cc4610d092bcd2&format=json";
+
+async function getRecentTracks() {
+  const response = await fetch(url);
+
+  const text = await response.text();
+
+  const json = JSON.parse(text);
+  const mostRecentTrack = json.recenttracks.track[0];
+
+  albumImage.src = mostRecentTrack.image[2]["#text"];
+
+  if (mostRecentTrack.hasOwnProperty("@attr"))
+    timeListened.innerText = "Now listening to:";
+  else timeListened.innerText = "Last song I listened to:";
+
+  recentTrackName.innerText = mostRecentTrack.name;
+  console.log(mostRecentTrack.name);
+  recentTrackArtist.innerText = mostRecentTrack.artist["#text"];
+
+  trackUrl.setAttribute("href", mostRecentTrack.url);
+}
+getRecentTracks();
+const lastFm = setInterval(getRecentTracks, 60000);
