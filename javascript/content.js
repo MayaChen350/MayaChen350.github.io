ad580df2aa60a8b91338f5d87ec5d4dc1e86cef8
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-03-18 11:13:56 -0400

    Beginning of the page content selector

--- javascript/content.js
+++ javascript/content.js
@@ -1,13 +1,54 @@
-const logos = Array.from(document.querySelectorAll(".logo"));
+// The content
+const content = document.querySelector(".content");
 
-const click = (element, unclick = false) =>
-  !unclick
-    ? element.classList.add("clicked")
-    : element.classList.remove("clicked");
+/////// LANGUAGE BIO ///////
+const languageContent = `<h2 class="pink">Hi I'm a nerd</h2>
 
-logos.forEach((logo, _, logos) =>
-  logo.addEventListener("click", () => {
-    logos.forEach((l) => click(l, true));
-    click(logo);
-  })
-);
+<div id="language-menu">
+  <img id="csharp" class="logo" src="images/logos/c-sharp.svg" alt="C# logo" />
+  <img id="c" class="logo" src="images/logos/C.svg" alt="C logo" />
+  <img id="html" class="logo" src="images/logos/HTML5.svg" alt="HTML logo" />
+  <img id="js" class="logo" src="images/logos/JavaScript.svg" alt="Js logo" />
+  <img id="python" class="logo" src="images/logos/Python.svg" alt="Python logo" />
+  <img id="lua" class="logo" src="images/logos/Lua.svg" alt="Lua logo" />
+  <img id="modding" class="logo" src="images/logos/Bepinex.svg" alt="Bepinex logo" />
+</div>
+
+<div class="description">
+  <h3 id="bonjour">Bonjour!</h3>
+  <div>
+    <p>I'm a programmer :p</p>
+    <p>
+      Right now I'm prefering low level languages and have crazy ideas
+      like... opening microwaves but you can still check what I can say
+      about the languages above
+    </p>
+    <br />
+    <p>Just click on a language!</p>
+  </div>
+</div>`;
+
+// Where the language content is going to be added
+const languageDescription = document.querySelector("#description");
+
+// Default content
+const contentDefault = `<h3 id="bonjour">Bonjour!</h3>
+<div>
+  <p>I'm a programmer :p</p>
+  <p>
+    Right now I'm prefering low level languages and have crazy ideas
+    like... opening microwaves but you can still check what I can say
+    about the languages above
+  </p>
+  <br />
+  <p>Just click on a language!</p>
+</div>`;
+
+// Language contents
+
+/////// THE PAGE ///////
+
+// Choose the content
+document
+    .querySelector("#btn-nerd-bio")
+    .addEventListener("click", () => (content.innerHTML = languageContent));
--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,12 @@
 const socialMedias = [
-  '<a id="social-media-username" class="yellow" href="https://github.com/MayaChen350">GITHUB: MayaChen350</a>',
-  '<a id="social-media-username" class="yellow" href="#">DISCORD: giratina_shiny</a>',
-  '<a id="social-media-username" class="yellow" href="https://www.last.fm/user/GChen3843">LAST.FM: GChen3843</a>',
+    '<a id="social-media-username" class="yellow" href="https://github.com/MayaChen350">GITHUB: MayaChen350</a>',
+    '<a id="social-media-username" class="yellow" href="#">DISCORD: giratina_shiny</a>',
+    '<a id="social-media-username" class="yellow" href="https://www.last.fm/user/GChen3843">LAST.FM: GChen3843</a>',
 ];
 const socialMediaPfps = [
-  "images/PFP/github.jfif",
-  "images/PFP/discord.webp",
-  "images/PFP/lastfm.png",
+    "images/PFP/github.jfif",
+    "images/PFP/discord.webp",
+    "images/PFP/lastfm.png",
 ];
 
 const mediaUsername = document.querySelector("#social-media-username");
@@ -14,9 +14,9 @@ const mediaPfp = document.querySelector("#pfp");
 let indexMedia = 0;
 
 const socialMediaChangeTimer = setInterval(() => {
-  indexMedia = indexMedia !== socialMedias.length - 1 ? indexMedia + 1 : 0;
-  mediaPfp.src = socialMediaPfps[indexMedia];
-  mediaUsername.innerHTML = socialMedias[indexMedia];
+    indexMedia = indexMedia !== socialMedias.length - 1 ? indexMedia + 1 : 0;
+    mediaPfp.src = socialMediaPfps[indexMedia];
+    mediaUsername.innerHTML = socialMedias[indexMedia];
 }, 10000);
 
 const dog = document.querySelector("#dog-picture");
@@ -25,20 +25,20 @@ const oikophobia = document.querySelector("#Oikophobia");
 let isPhobiaReal = false;
 
 oikophobia.addEventListener("mouseout", () => {
-  dog.style.opacity = 1;
-  isPhobiaReal = false;
+    dog.style.opacity = 1;
+    isPhobiaReal = false;
 });
 
 oikophobia.addEventListener("mouseenter", () => {
-  dog.style.opacity = 1;
-  isPhobiaReal = true;
-  setTimeout(() => {
-    let int = setInterval(() => {
-      if (dog.style.opacity !== 0 && isPhobiaReal === true)
-        dog.style.opacity -= 0.003;
-      else clearInterval(int);
-    }, 100);
-  }, 5000);
+    dog.style.opacity = 1;
+    isPhobiaReal = true;
+    setTimeout(() => {
+        let int = setInterval(() => {
+            if (dog.style.opacity !== 0 && isPhobiaReal === true)
+                dog.style.opacity -= 0.003;
+            else clearInterval(int);
+        }, 100);
+    }, 5000);
 });
 
 // Last song listened to, powered by Last.fm (My beloved)
@@ -50,27 +50,27 @@ const recentTrackArtist = document.querySelector("#recent-song-artist");
 const trackUrl = document.querySelector("#track-url");
 
 const url =
-  "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=GChen3843&api_key=d7b26ab026668c5044cc4610d092bcd2&format=json";
+    "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=GChen3843&api_key=d7b26ab026668c5044cc4610d092bcd2&format=json";
 
 async function getRecentTracks() {
-  const response = await fetch(url);
+    const response = await fetch(url);
 
-  const text = await response.text();
+    const text = await response.text();
 
-  const json = JSON.parse(text);
-  const mostRecentTrack = json.recenttracks.track[0];
+    const json = JSON.parse(text);
+    const mostRecentTrack = json.recenttracks.track[0];
 
-  albumImage.src = mostRecentTrack.image[2]["#text"];
+    albumImage.src = mostRecentTrack.image[2]["#text"];
 
-  if (mostRecentTrack.hasOwnProperty("@attr"))
-    timeListened.innerText = "Now listening to:";
-  else timeListened.innerText = "Last song I listened to:";
+    if (mostRecentTrack.hasOwnProperty("@attr"))
+        timeListened.innerText = "Now listening to:";
+    else timeListened.innerText = "Last song I listened to:";
 
-  recentTrackName.innerText = mostRecentTrack.name;
-  console.log(mostRecentTrack.name);
-  recentTrackArtist.innerText = mostRecentTrack.artist["#text"];
+    recentTrackName.innerText = mostRecentTrack.name;
+    console.log(mostRecentTrack.name);
+    recentTrackArtist.innerText = mostRecentTrack.artist["#text"];
 
-  trackUrl.setAttribute("href", mostRecentTrack.url);
+    trackUrl.setAttribute("href", mostRecentTrack.url);
 }
 getRecentTracks();
 const lastFm = setInterval(getRecentTracks, 60000);
@@ -93,12 +93,27 @@ document.querySelector("style").innerHTML = `
 // determine if night or day 🤓
 const currentHour = new Date().getHours();
 let bonjourBonsoir =
-  currentHour > 5 && currentHour < 18
-    ? () => {
-        document.querySelector("#bonjour").innerText = "Bonjour!";
-      }
-    : () => {
-        document.querySelector("#bonjour").innerText = "Bonsoir!";
-      };
+    currentHour > 5 && currentHour < 18
+        ? () => {
+              document.querySelector("#bonjour").innerText = "Bonjour!";
+          }
+        : () => {
+              document.querySelector("#bonjour").innerText = "Bonsoir!";
+          };
 
 bonjourBonsoir();
+
+// Make the logo stay big
+const logos = Array.from(document.querySelectorAll(".logo"));
+
+const click = (element, unclick = false) =>
+    !unclick
+        ? element.classList.add("clicked")
+        : element.classList.remove("clicked");
+
+logos.forEach((logo, _, logos) =>
+    logo.addEventListener("click", () => {
+        logos.forEach((l) => click(l, true));
+        click(logo);
+    })
+);
