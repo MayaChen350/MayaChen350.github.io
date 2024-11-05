44dc3cc60ec34e4a374abb6204d90b51ddb9114d
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-11-04 21:25:03 -0500

    Import of the last fm live status
    But with React

--- src/components/home/lastFmStatus.jsx
+++ src/components/home/lastFmStatus.jsx
@@ -1,44 +1,75 @@
 import React from "react";
 import "./_lastFmStatus.scss";
+import { useState, useEffect } from "react";
 
 export default function LastFmStatus() {
-    // const url =
-    //     "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=GChen3843&api_key=d7b26ab026668c5044cc4610d092bcd2&format=json";
+    const [recentTrack, setRecentTrack] = useState({
+        timeListened: "",
+        albumImage: "",
+        trackUrl: "",
+        recentTrackName: "",
+        recentTrackArtist: "",
+    });
 
-    // let timeListened;
+    useEffect(() => {
+        let ignore = false;
+        fetch(
+            "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=GChen3843&api_key=d7b26ab026668c5044cc4610d092bcd2&format=json"
+        )
+            .then((response) => response.text())
+            .then((text) => {
+                if (!ignore) {
+                    const json = JSON.parse(text);
 
-    // async function getRecentTracks() {
-    //     const response = await fetch(url);
+                    const mostRecentTrack = json?.recenttracks?.track[0];
 
-    //     const text = await response.text();
+                    const albumImage = mostRecentTrack?.image[2]["#text"];
 
-    //     const json = JSON.parse(text);
-    //     const mostRecentTrack = json.recenttracks.track[0];
+                    let timeListened;
 
-    //     albumImage.src = mostRecentTrack.image[2]["#text"];
+                    if (mostRecentTrack?.hasOwnProperty("@attr"))
+                        timeListened = "Now listening to:";
+                    else timeListened = "Last song I listened to:";
 
-    //     if (mostRecentTrack.hasOwnProperty("@attr"))
-    //         timeListened = "Now listening to:";
-    //     else timeListened = "Last song I listened to:";
+                    const recentTrackName = mostRecentTrack?.name;
+                    console.log(mostRecentTrack?.name);
+                    const recentTrackArtist = mostRecentTrack?.artist["#text"];
 
-    //     recentTrackName.innerText = mostRecentTrack.name;
-    //     console.log(mostRecentTrack.name);
-    //     recentTrackArtist.innerText = mostRecentTrack.artist["#text"];
+                    const trackUrl = mostRecentTrack?.url;
 
-    //     trackUrl.setAttribute("href", mostRecentTrack.url);
-    //     start();
-    // }
-    // getRecentTracks();
+                    setRecentTrack({
+                        timeListened: timeListened,
+                        albumImage: albumImage,
+                        trackUrl: trackUrl,
+                        recentTrackName: recentTrackName,
+                        recentTrackArtist: recentTrackArtist,
+                    });
+                }
+            });
+        return () => {
+            ignore = true;
+        };
+    });
 
     return (
         <aside className="last-fm-status">
-            <img id="album-image" src="images/svg/loading.svg" alt="loading" />
+            <img
+                id="album-image"
+                src={recentTrack?.albumImage ?? "/images/loading.svg"}
+                alt="loading"
+            />
             <div>
-                <p id="time-of-listen">Last song I listened to:</p>
-                <a id="track-url">
-                    <p id="recent-song-track">Loading song...</p>
+                <p id="time-of-listen">
+                    {recentTrack?.timeListened ?? "Last song I listened to:"}
+                </p>
+                <a id="track-url" href={recentTrack?.trackUrl ?? ""}>
+                    <p id="recent-song-track">
+                        {recentTrack?.recentTrackName ?? "Loading song..."}
+                    </p>
                 </a>
-                <p id="recent-song-artist">Loading artist...</p>
+                <p id="recent-song-artist">
+                    {recentTrack?.recentTrackArtist ?? "Loading artist..."}
+                </p>
             </div>
         </aside>
     );
