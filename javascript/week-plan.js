a03b9fbe4153857b0971d1c4369fb741880e85b7
Maya <145165822+MayaChen350@users.noreply.github.com>
2025-10-03 20:27:24 -0400

    INIT Kobweb

---
+++ javascript/week-plan.js
@@ -0,0 +1,18 @@
+const horribleIdeas = [
+	"Everything is going to be okay",
+	"It's hacking time",
+	"'Cause hell is forever",
+	"Free, at last!",
+	"Oikophobia",
+	"Resurrection",
+	"Climb Celeste mountain",
+	"Rewrite my OS in Haskell",
+	"Convert my friends to Lisp",
+	"Announce my unified theory",
+	"Buy a box of Crayolas to feast on",
+	"Get in trial for false murder accusations again",
+	"Put God in a ball",
+	"Remember you're genocides",
+];
+
+document.querySelector("availabilities").appendChild()
