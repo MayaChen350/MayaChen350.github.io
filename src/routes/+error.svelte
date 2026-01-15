8adb6cfca8c1cb8f544a88b8990aa5c00bff7cfd
Maya <145165822+MayaChen350@users.noreply.github.com>
2026-01-14 23:08:43 -0500

    allow pages to be case insensitive

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {browser} from "$app/environment";
+    import {onMount} from "svelte";
+
+    let visible = $state(false);
+
+    if (browser) {
+        onMount(() => {
+            const currentURL = window.location.href;
+            const lowerCaseURL = currentURL.toLowerCase();
+            if (currentURL !== lowerCaseURL) {
+                location.replace(lowerCaseURL);
+            } else if (window.location.pathname.endsWith("mayascope")) {
+                location.pathname = "/Mayascope";
+            } else {
+                visible = true
+            }
+        })
+    }
+</script>
+
+
+<h1 style="visibility: {visible ? 'visible' : 'hidden'}">Error page!</h1>
